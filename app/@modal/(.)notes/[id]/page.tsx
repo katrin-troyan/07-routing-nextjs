e19f40432f9453324631332302243649c2f773d7
Kateryna Troyan <ket.tro@gmail.com>
2025-10-06 18:45:37 +0300

    fix NotePreview.client.tsx

--- app/@modal/(.)notes/[id]/page.tsx
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -1,42 +1,7 @@
-"use client";
+import NotePreview from "./NotePreview.client";
 
-import { useQuery } from "@tanstack/react-query";
-import { useParams } from "next/navigation";
-import { fetchNoteById } from "@/lib/api";
-import Modal from "@/components/Modal/Modal";
-import css from "./NotePreview.module.css";
-
-const NotePreview = () => {
-  const { id } = useParams<{ id: string }>();
-
-  const {
-    data: note,
-    isLoading,
-    error,
-  } = useQuery({
-    queryKey: ["note", id],
-    queryFn: () => fetchNoteById(id),
-    refetchOnMount: false,
-  });
-
-  if (isLoading) return <p>Loading, please wait...</p>;
-
-  if (error || !note) return <p>Something went wrong.</p>;
-
-  return (
-    <Modal onClose={() => {}}>
-      <div className={css.container}>
-        <div className={css.item}>
-          <div className={css.header}>
-            <h2>{note.title}</h2>
-          </div>
-          <p className={css.content}>{note.content}</p>
-          <p className={css.date}>{note.createdAt}</p>
-          <p className={css.tag}>{note.tag}</p>
-        </div>
-      </div>
-    </Modal>
-  );
+const NotePreviewPage = () => {
+  return <NotePreview />;
 };
 
-export default NotePreview;
+export default NotePreviewPage;
